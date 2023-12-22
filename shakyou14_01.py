ebfd9798cf7e5328029c71cc367376ec361326ac
KazzzTen <tengoo1999@gmail.com>
2023-12-22 16:35:57 +0900

    第14章 クラス変数とインスタンス変数
    ・クラス変数は各インスタンスから参照できる

--- shakyou14_01.py
+++ shakyou14_01.py
@@ -1,13 +1,27 @@
 # 写経14章 クラス変数とインスタンス変数
 
 class Rectangle:
+    recs = []  # クラス変数
+
     def __init__(self, w, l):
         self.width = w  # インスタンス変数
         self.length = l  # インスタンス変数
+        self.recs.append((self.width, self.length))  # インスタンスが生成れるたびに、クラス変数に追加を行う
 
     def print_size(self):
         print("{} by {}".format(self.width, self.length))
 
 
-my_rectangle = Rectangle(w=10, l=25)
-my_rectangle.print_size()
+# my_rectangle = Rectangle(w=10, l=25)
+# my_rectangle.print_size()
+r1 = Rectangle(w=10, l=25)
+r1.print_size()
+print(r1.recs)
+r2 = Rectangle(w=30, l=10)
+r2.print_size()
+print(r2.recs)
+r3 = Rectangle(w=20, l=15)
+r3.print_size()
+print(r3.recs)
+
+print(Rectangle.recs)  # クラス変数の出力
