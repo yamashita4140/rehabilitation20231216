46b478641b1ee49b6fea1cdd0ca2b9782982d853
KazzzTen <tengoo1999@gmail.com>
2023-12-22 15:42:44 +0900

    第13章 チャレンジ1 -> 3

--- challenge/challenge13_01.py
+++ challenge/challenge13_01.py
@@ -1,18 +1,27 @@
-# 第13章 チャレンジ1
+# 第13章 チャレンジ1 -> チャレンジ3
 
-class Rectangle:
+class Shape:
     def __init__(self, w, l):
         self.width = w
         self.length = l
+
+    def what_am_i(self):
+        return "I am a shape"
+
+
+class Rectangle(Shape):
+    # def __init__(self, w, l):
+    #     self.width = w
+    #     self.length = l
 
     def calculate_perimeter(self):
         return self.width * 2 + self.length * 2
 
 
-class Square:
-    def __init__(self, w, l):
-        self.width = w
-        self.length = l
+class Square(Shape):
+    # def __init__(self, w, l):
+    #     self.width = w
+    #     self.length = l
 
     def calculate_perimeter(self):
         return self.width * 2 + self.length * 2
@@ -20,5 +29,7 @@ class Square:
 
 rectangle = Rectangle(w=15, l=20)
 print(rectangle.calculate_perimeter())
+print(rectangle.what_am_i())
 square = Square(w=20, l=20)
 print(square.calculate_perimeter())
+print(square.what_am_i())
