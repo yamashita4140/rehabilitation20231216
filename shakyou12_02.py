5f48895bca87d8418916c7abb12971a27a8310e2
KazzzTen <tengoo1999@gmail.com>
2023-12-22 10:39:26 +0900

    第12章 プログラミングパラダイム オブジェクト指向型プログラミング ・インスタンス変数の更新

--- shakyou12_02.py
+++ shakyou12_02.py
@@ -10,6 +10,11 @@ class Orange:  # Orangeクラスの定義
 
 # クラスのインスタンス化
 orl = Orange(w=10, c="dark orange")  # インスタンス化の際に、weightとcolorに初期値を与えている
-print(orl)
+# print(orl)
 print(orl.weight)  # インスタンス変数 weightの初期値表示
 print(orl.color)  # インスタンス変数 colorの初期値表示
+
+orl.weight = 100  # インスタンス変数 weightの更新
+orl.color = "light orange"  # インスタンス変数 colorの更新
+print(orl.weight)  # 更新したインスタンス変数 weightの値の表示
+print(orl.color)  # 更新したインスタンス変数 colorの値の表示
