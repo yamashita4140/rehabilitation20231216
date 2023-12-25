9611840ffd2f57049d57ba97e691d01f703a94b9
KazzzTen <tengoo1999@gmail.com>
2023-12-25 10:48:03 +0900

    第15章 戦争ゲーム ・Class Player の作成 ・Class Game の作成

--- the_war_card/war.py
+++ the_war_card/war.py
@@ -51,3 +51,60 @@ class Deck:
         if len(self.cards) == 0:
             return
         return self.cards.pop()
+
+
+class Player:
+    def __init__(self, name):
+        self.wins = 0
+        self.card = None
+        self.name = name
+
+
+class Game:
+    def __init__(self):
+        name1 = input("Player1 Name: ")
+        name2 = input("Player2 Name: ")
+        self.deck = Deck()
+        self.p1 = Player(name1)
+        self.p2 = Player(name2)
+
+    def wins(self, winner):
+        w = "{} won this round."
+        w = w.format(winner)
+        print(w)
+
+    def draw(self, p1n, p1c, p2n, p2c):
+        d = "{} draw {} and {} draw {}."
+        d = d.format(p1n, p1c, p2n, p2c)
+        print(d)
+
+    def play_game(self):
+        cards = self.deck.cards
+        print("Start the war!!")
+
+        while len(cards) >= 2:
+            m = "If 'q' is entered, cease fire; any other key will continue the battle."
+            response = input(m)
+            if response == "q":
+                break
+            p1c = self.deck.rm_card()
+            p2c = self.deck.rm_card()
+            p1n = self.p1.name
+            p2n = self.p2.name
+            self.draw(p1n, p1c, p2n, p2c)
+            if p1c > p2c:
+                self.p1.wins += 1
+                self.wins(self.p1.name)
+            else:
+                self.p2.wins += 1
+                self.wins(self.p2.name)
+
+        win = self.winners(self.p1, self.p2)
+        print("The war is over...{} is winning!".format(win))
+
+    def winner(self, p1, p2):
+        if p1.wins > p2.wins:
+            return p1.name
+        if p2.wins > p1.wins:
+            return p2.name
+        return "Draw!!"
