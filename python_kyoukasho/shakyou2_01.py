3624d13f3ce4f92cdc4094b0216faa78954326bf
KazzzTen <tengoo1999@gmail.com>
2024-01-05 11:19:05 +0900

    第15章 戦争ゲーム ・コードの修正

---
+++ python_kyoukasho/shakyou2_01.py
@@ -0,0 +1,54 @@
+# Pythonの教科書 2章
+
+# 東京大阪まで何時間?
+print("東京大阪まで何時間?")
+kyori = 507.5
+jisoku = 80
+jikan = kyori / jisoku
+print(jikan)
+
+# インチをセンチメートルに変換
+print("インチをセンチメートルに変換")
+per_inch = 2.54
+inch = 8
+cm = inch * per_inch
+print(cm)
+
+# 数値に説明を付加
+print("数値に説明を付加")
+age = 18
+print("年齢は", age, "歳です。")
+
+# print()で改行しない方法
+print("print()で改行しない方法")
+print("剣で突き刺すかのように", end="")
+print("話す者がいる。", end="")
+print("しかし賢い者たちの舌は", end="")
+print("人を癒やす")
+
+# 台形の面積計算
+print("台形の面積計算")
+upper = 2
+lower = 3
+height = 4
+area = (upper + lower) * height / 2
+print("台形の面積は", area, "cm2")
+
+# 花屋の支払金額を求める
+print("花屋の支払金額を求める")
+# 値段
+rose_v = 500
+sun_v = 400
+tulip_v = 700
+
+# 個数
+rose_c = 18
+sun_c = 8 - 2
+tulip_c = 21 - 5
+
+# 割引率
+rate = 0.9
+
+# 計算
+sum_v = (rose_v)
+# 結果表示
